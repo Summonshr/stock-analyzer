f635f472de8d6877f8fecf0749a8d4a1624240b7
suman shrestha <summonshr@pop-os.localdomain>
2020-11-10 10:18:25 +0545

    WIP

--- src/App.js
+++ src/App.js
@@ -7,6 +7,40 @@ function Grahams({ bvps, eps }) {
   return bvps && eps && bvps > 0 && eps > 0 ? (Math.sqrt(22.5 * bvps * eps)).toFixed(2) : ''
 }
 
+function getClassLowerBetter(v,p1,p2,p3,p4){
+  if(v<=0){
+    return ''
+  }
+  if(v < p1){
+    return 'bg-green'
+  }
+  if(v < p2){
+    return 'bg-green-light'
+  }
+  if(v < p3){
+    return 'bg-green-lighter'
+  }
+  if(v < p4){
+    return 'bg-green-lightest'
+  }
+}
+
+function getClassHigherBetter(v,p1,p2,p3,p4){
+  
+  if(v > p4){
+    return 'bg-green'
+  }
+  if(v > p3){
+    return 'bg-green-light'
+  }
+  if(v > p2){
+    return 'bg-green-lighter'
+  }
+  if(v > p1){
+    return 'bg-green-lightest'
+  }
+}
+
 function Company({ company }) {
 
   let [data, setData] = useState('')
@@ -46,26 +80,18 @@ function Company({ company }) {
     let pe = eps ? (latest.latestPrice / eps).toFixed(2) : 0
     let price = latest.latestPrice
     let change = (fair_value - price).toFixed()
-    if (change > 0) {
-      className.push('bg-green')
-    } else if (change / price > -0.05) {
-      className.push('bg-green-light')
-    } else if (change / price > -0.10) {
-      className.push('bg-green-lighter')
-    } else if (change / price > -0.15) {
-      className.push('bg-green-lightest')
-    }
-    return <tr className={className.join(' ')} key={company.ticker}><td>{company.ticker}</td><td>{company.companyName}</td>
+  
+    return <tr key={company.ticker}><td>{company.ticker}</td><td>{company.companyName}</td>
       {latest && <>
         <td>{price}</td>
         <td>{latest.pointChange} {latest.pointChange > 0 ? '↑' : '↓'}</td>
       </>}
       {data.keyFinancial && <>
-        <td>{eps.toFixed(2)}</td>
-        <td>{pe}</td>
-        <td>{bvps.toFixed(2)}</td>
+        <td className={getClassHigherBetter(eps,15,20,30, 45)}>{eps.toFixed(2)}</td>
+        <td className={getClassLowerBetter(pe,10,15,20,25)}>{pe}</td>
+        <td className={getClassHigherBetter(bvps, 150,200,250,300)}>{bvps}</td>
         <td>{fair_value}</td>
-        <td>{(change / price).toFixed(2)}</td>
+        <td className={getClassHigherBetter((change / price).toFixed(2), -0.40, -0.30, -0.20, -0.10)}>{(change / price).toFixed(2)}</td>
       </>}
     </tr>
   }
