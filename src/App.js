988f35ea28dfa293d2e2e9d148fda40d36b821aa
Suman Shrestha <summonshr@gmail.com>
2020-11-19 18:03:14 +0545

    WIP

--- src/App.js
+++ src/App.js
@@ -7,38 +7,38 @@ function Grahams({ bvps, eps }) {
   return bvps && eps && bvps > 0 && eps > 0 ? (Math.sqrt(22.5 * bvps * eps)).toFixed(2) : ''
 }
 
-function getClassLowerBetter(v,p1,p2,p3,p4){
-  if(v<=0){
+function getClassLowerBetter(v, p1, p2, p3, p4) {
+  if (v <= 0) {
     return ''
   }
-  if(v < p1){
+  if (v < p1) {
     return 'bg-green'
   }
-  if(v < p2){
+  if (v < p2) {
     return 'bg-green-light'
   }
-  if(v < p3){
+  if (v < p3) {
     return 'bg-green-lighter'
   }
-  if(v < p4){
+  if (v < p4) {
     return 'bg-green-lightest'
   }
 }
 
-function getClassHigherBetter(v,p1,p2,p3,p4){
-  if(pe <= 0 ){
+function getClassHigherBetter(v, p1, p2, p3, p4) {
+  if (v <= 0) {
     return ''
   }
-  if(v > p4){
+  if (v > p4) {
     return 'bg-green'
   }
-  if(v > p3){
+  if (v > p3) {
     return 'bg-green-light'
   }
-  if(v > p2){
+  if (v > p2) {
     return 'bg-green-lighter'
   }
-  if(v > p1){
+  if (v > p1) {
     return 'bg-green-lightest'
   }
 }
@@ -49,7 +49,7 @@ function Company({ company }) {
   let [latest, setLatest] = useState('')
 
   useEffect(() => {
-    
+
     if (localStorage.getItem('ticker-' + company.ticker)) {
       setData(JSON.parse(localStorage.getItem('ticker-' + company.ticker)))
     } else {
@@ -78,20 +78,20 @@ function Company({ company }) {
   }
   if (data && data.keyFinancial && data.keyFinancial.data && data.keyFinancial.data[0]) {
     let { eps, bvps } = data.keyFinancial.data[0]
-    let fair_value = Grahams(data.keyFinancial.data[0])
-    let pe = eps ? (latest.latestPrice / eps).toFixed(2) : 0
-    let price = latest.latestPrice
-    let change = (fair_value - price).toFixed()
-  
-    return <tr key={company.ticker}><td>{company.ticker}</td><td>{company.companyName}</td>
+    let fair_value    = Grahams(data.keyFinancial.data[0])
+    let pe            = eps ? (latest.latestPrice / eps).toFixed(2) : 0
+    let price         = latest.latestPrice
+    let change        = (fair_value - price).toFixed()
+
+    return <tr key={company.ticker}><td><a href={"https://sharesansar.com/company/"+company.ticker} target="_blank">{company.ticker}</a></td><td>{company.companyName}</td>
       {latest && <>
         <td>{price}</td>
         <td>{latest.pointChange} {latest.pointChange > 0 ? '↑' : '↓'}</td>
       </>}
       {data.keyFinancial && <>
-        <td className={getClassHigherBetter(eps,15,20,30, 45)}>{eps.toFixed(2)}</td>
-        <td className={getClassLowerBetter(pe,10,15,20,25)}>{pe}</td>
-        <td className={getClassHigherBetter(bvps, 150,200,250,300)}>{bvps}</td>
+        <td className={getClassHigherBetter(eps, 15, 20, 30, 45)}>{eps.toFixed(2)}</td>
+        <td className={getClassLowerBetter(pe, 10, 15, 20, 25)}>{pe}</td>
+        <td className={getClassHigherBetter(bvps, 150, 200, 250, 300)}>{bvps}</td>
         <td>{fair_value}</td>
         <td className={getClassHigherBetter((change / price).toFixed(2), -0.40, -0.30, -0.20, -0.10)}>{(change / price).toFixed(2)}</td>
       </>}
